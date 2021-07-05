1669556d3f69e5c21f11ea2afe6ccb7ed438847d
Nathan Martin <nathanrm262@gmail.com>
2021-07-04 19:34:34 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+import re
+import base64
+import requests
+import os
+import json
+
+from shared_flow import send_token_request
+from shared_flow import handle_sso_token_response
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("ESI_API_SECRET_KEY")
+client_id = os.environ.get("ESI_CLIENT_ID")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", value=client_id)
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/callback/", methods=['GET', 'POST'])
+def callback():
+    code = request.args.get('code')
+
+    user_pass = "{}:{}".format(client_id, app.secret_key)
+    basic_auth = base64.urlsafe_b64encode(user_pass.encode('utf-8')).decode()
+    auth_header = "Basic {}".format(basic_auth)
+
+    form_values = {
+        "grant_type": "authorization_code",
+        "code": code,
+    }
+
+    headers = {"Authorization": auth_header}
+    res = send_token_request(form_values, add_headers=headers)
+
+    data = handle_sso_token_response(res)
+
+    print(data)
+    return render_template("callback.html", data=data)
---
+++ shared_flow.py
@@ -0,0 +1,92 @@
+"""Contains all shared OAuth 2.0 flow functions for examples
+
+This module contains all shared functions between the two different OAuth 2.0
+flows recommended for web based and mobile/desktop applications. The functions
+found here are used by the OAuth 2.0 examples contained in this project.
+"""
+import urllib
+
+import requests
+
+from validate_jwt import validate_eve_jwt
+
+
+def send_token_request(form_values, add_headers={}):
+    """Sends a request for an authorization token to the EVE SSO.
+
+    Args:
+        form_values: A dict containing the form encoded values that should be
+                     sent with the request
+        add_headers: A dict containing additional headers to send
+    Returns:
+        requests.Response: A requests Response object
+    """
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Host": "login.eveonline.com",
+    }
+
+    if add_headers:
+        headers.update(add_headers)
+
+    res = requests.post(
+        "https://login.eveonline.com/v2/oauth/token",
+        data=form_values,
+        headers=headers,
+    )
+
+    print("Request sent to URL {} with headers {} and form values: "
+          "{}\n".format(res.url, headers, form_values))
+    res.raise_for_status()
+
+    return res
+
+
+def handle_sso_token_response(sso_response):
+    """Handles the authorization code response from the EVE SSO.
+
+    Args:
+        sso_response: A requests Response object gotten by calling the EVE
+                      SSO /v2/oauth/token endpoint
+    """
+
+    if sso_response.status_code == 200:
+        data = sso_response.json()
+        access_token = data["access_token"]
+
+        print("\nVerifying access token JWT...")
+
+        jwt = validate_eve_jwt(access_token)
+        character_id = jwt["sub"].split(":")[2]
+        character_name = jwt["name"]
+        publicData_path = ("https://esi.evetech.net/latest/characters/{}"
+                           "/".format(character_id))
+
+        print("\nSuccess! Here is the payload received from the EVE SSO: {}"
+              "\nYou can use the access_token to make an authenticated "
+              "request to {}".format(data, publicData_path))
+
+        headers = {
+            "Authorization": "Bearer {}".format(access_token)
+        }
+
+        res = requests.get(publicData_path, headers=headers)
+        print("\nMade request to {} with headers: "
+              "{}".format(publicData_path, res.request.headers))
+        res.raise_for_status()
+
+        data = res.json()
+        return data
+    else:
+        print("\nSomething went wrong! Re read the comment at the top of this "
+              "file and make sure you completed all the prerequisites then "
+              "try again. Here's some debug info to help you out:")
+        print("\nSent request with url: {} \nbody: {} \nheaders: {}".format(
+            sso_response.request.url,
+            sso_response.request.body,
+            sso_response.request.headers
+        ))
+        print("\nSSO response code is: {}".format(sso_response.status_code))
+        print("\nSSO response JSON is: {}".format(sso_response.json()))
+        return -1
