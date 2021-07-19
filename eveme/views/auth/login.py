65aefa6afded1949572e88fdacd4383d2ae70369
nathmart <nathmart@umich.edu>
2021-07-18 22:26:43 -0700

    Pulls max/min price for buy/sell orders respect.

--- eveme/views/auth/login.py
+++ eveme/views/auth/login.py
@@ -87,7 +87,6 @@ def character(char_id):
                 buyOrders[i] = '{'+buyOrders[i]
             else:
                 buyOrders[i] = '{'+buyOrders[i]+'}'
-            # print(buyOrders[i])
             buyOrdersDicts.append(json.loads(buyOrders[i]))
         for i in range(len(sellOrders)):
             if i == 0:
@@ -96,9 +95,7 @@ def character(char_id):
                 sellOrders[i] = '{'+sellOrders[i]
             else:
                 sellOrders[i] = '{'+sellOrders[i]+'}'
-            # print(sellOrders[i])
             sellOrdersDicts.append(json.loads(sellOrders[i]))
-        # print(buyOrders[0])
         output['name'] = current_user.name
         output['profile_pic'] = current_user.profile_pic
         output['buyOrders'] = buyOrdersDicts
@@ -132,7 +129,6 @@ def callback():
     context = {}
     json_url = os.path.join(pathlib.Path().resolve(), "eveme/static/json", "invTypes.json")
     invTypes = dict(json.load(open(json_url)))
-    # print(invTypes['11971'])
     code = request.args.get('code')
 
     user_pass = "{}:{}".format(current_app.config['ESI_CLIENT_ID'],
@@ -150,16 +146,54 @@ def callback():
     char_id = data['id']
     userBuyOrders = []
     userSellOrders = []
+    structuresChecked = {}
+
+    headers = eveme.helper.createHeaders(data['access_token'])
 
     for order in data['orders']:
         eveme.helper.insertStructure(char_id, order['location_id'])
+        if order['location_id'] in structuresChecked.keys():
+            structureOrders = structuresChecked[order['location_id']]
+        else:
+            structureOrdersQuery = ("https://esi.evetech.net/latest/markets/structures"
+                                    "/{}/".format(order['location_id']))
+            res = requests.get(structureOrdersQuery, headers=headers)
+            res.raise_for_status()
+            numPages = res.headers['X-Pages']
+            structureOrders = res.json()
+
+            for i in range(1, int(numPages)):
+                structureOrdersQuery = ("https://esi.evetech.net/latest/markets/structures"
+                                        "/{}/?datasource=tranquility&page="
+                                        "{}".format(order['location_id'], i + 1))
+                res = requests.get(structureOrdersQuery, headers=headers)
+                numPages = res.headers['X-Pages']
+                structureOrders.extend(res.json())
+            structuresChecked[order['location_id']] = structureOrders
+
+        # check each order with order in structure and compare
         if 'is_buy_order' in order.keys():
             order['itemName'] = invTypes[str(order['type_id'])]
+            orderMaxPrice = -1
+            for structOrder in structureOrders:
+                # bug here string indices?
+                if (structOrder['is_buy_order']) and (structOrder['type_id'] == order['type_id']):
+                    print(structOrder['price'], 'buy')
+                    if structOrder['price'] > orderMaxPrice:
+                        orderMaxPrice = structOrder['price']
+            order['structureHighest'] = orderMaxPrice
             order.pop('type_id', None)
             order.pop('location_id', None)
             userBuyOrders.append(json.dumps(order))
         else:
             order['itemName'] = invTypes[str(order['type_id'])]
+            orderMinPrice = float('inf')
+            for structOrder in structureOrders:
+                if (not structOrder['is_buy_order']) and (structOrder['type_id'] == order['type_id']):
+                    print(structOrder['price'], 'sell')
+                    if structOrder['price'] < orderMinPrice:
+                        orderMinPrice = structOrder['price']
+            order['structureLowest'] = orderMinPrice
             order.pop('type_id', None)
             order.pop('location_id', None)
             userSellOrders.append(json.dumps(order))
@@ -172,14 +206,13 @@ def callback():
     # Create a user in your db with the information provided
     # by ESI
 
-    # print(userSellOrders, userBuyOrders)
     buyOrders = ','.join(userBuyOrders)
     sellOrders = ','.join(userSellOrders)
     user = User(
         id_=char_id, name_=data['name'], profile_pic_=picture,
         buyOrders_=buyOrders, sellOrders_=sellOrders, access_token_=data['access_token']
     )
-    # print(user.buyOrders)
+
     login_user(user)
     # Doesn't exist? Add it to the database.
     if not User.get(char_id):
