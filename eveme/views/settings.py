88cf18db6ad47d6a78bb3c3deda8ebf56f33629e
nathmart <nathmart@umich.edu>
2021-07-28 21:49:19 -0700

    Settings page updated

--- eveme/views/settings.py
+++ eveme/views/settings.py
@@ -8,8 +8,12 @@ import flask
 import eveme
 
 
-@eveme.app.route('/settings/')
+@eveme.app.route('/settings/', methods=['GET', 'POST'])
 def show_settings():
-    """Display / route."""
+    """Display /settings/ route and pull any scopes that need to be refreshed."""
     context = {}
-    return flask.render_template("settings.html", **context)
+
+    if flask.request.method == 'POST':
+        return flask.render_template("settings.html", **context)
+
+    return flask.render_template("settings.html")
