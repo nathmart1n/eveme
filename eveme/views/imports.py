aacd4551df4b215812e49b8bc9aaf9a413332e78
nathmart <nathmart@umich.edu>
2021-08-05 12:05:01 -0700

    Started importing stuff

--- eveme/views/imports.py
+++ eveme/views/imports.py
@@ -8,17 +8,22 @@ import flask
 import eveme
 import eveme.helper
 from flask_login import current_user
+from firebase_admin import db
 
 
 @eveme.app.route('/imports/', methods=['GET', 'POST'])
 def show_imports():
     """Display /imports/ route."""
+
     context = {}
+    context['isPost'] = False
 
     if flask.request.method == 'POST':
-        
-        return flask.render_template("imports.html", **context)
+        context['isPost'] = True
+        return flask.render_template("imports.html", context=context)
+
+    context['structures'] = db.reference('users/' + current_user.id + '/structureAccess').get()
 
     if current_user.is_authenticated:
         eveme.helper.refreshAuth()
-    return flask.render_template("imports.html", **context)
+    return flask.render_template("imports.html", context=context)
