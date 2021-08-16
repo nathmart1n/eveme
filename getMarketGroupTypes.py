f8d789694b65a20026cb19f1e2a5f16bde7f4603
nathmart <nathmart@umich.edu>
2021-08-16 16:24:45 -0700

    Working on building order group tree

---
+++ getMarketGroupTypes.py
@@ -0,0 +1,30 @@
+"""
+Helper python file to refresh typeIDs JSON
+"""
+import requests
+import pandas as pd
+import json
+import os
+import pathlib
+import sys
+
+print('Beginning file download with requests')
+
+json_url = os.path.join(pathlib.Path().resolve(), "eveme/static/json", "invTypes.json")
+invTypes = dict(json.load(open(json_url)))
+
+url = 'https://www.fuzzwork.co.uk/dump/latest/invTypes.csv'
+r = requests.get(url)
+
+with open('invTypes.csv', 'wb') as f:
+    f.write(r.content)
+
+df = pd.read_csv('invTypes.csv')
+
+df_copy = df[df['marketGroupID'] == '1696']
+print(df_copy)
+
+marketGroupTypes = df.groupby('marketGroupID')['typeName'].apply(list).to_dict()
+
+with open('eveme/static/json/marketGroupTypes.json', 'w') as fp:
+    json.dump(marketGroupTypes, fp, indent=4, sort_keys=True)
--- getMarketGroups.py
+++ getMarketGroups.py
@@ -6,19 +6,12 @@ import pandas as pd
 import json
 import os
 import pathlib
-
-
-def subtree(node, relationships):
-    return {
-        v: subtree(v, relationships)
-        for v in [x[0] for x in relationships if x[1] == node]
-    }
-
+import sys
 
 print('Beginning file download with requests')
 
-json_url = os.path.join(pathlib.Path().resolve(), "eveme/static/json", "invTypes.json")
-invTypes = dict(json.load(open(json_url)))
+json_url = os.path.join(pathlib.Path().resolve(), "eveme/static/json", "marketGroupTypes.json")
+marketGroupTypes = dict(json.load(open(json_url)))
 
 url = 'https://www.fuzzwork.co.uk/dump/latest/invMarketGroups.csv'
 r = requests.get(url)
@@ -26,18 +19,46 @@ r = requests.get(url)
 with open('marketGroups.csv', 'wb') as f:
     f.write(r.content)
 
-
 df_marketGroups = pd.read_csv('marketGroups.csv')
+idToName = pd.Series(df_marketGroups['marketGroupName'].values, index=df_marketGroups['marketGroupID']).to_dict()
+idToName = {str(key): value for key, value in idToName.items()}
+idToName['None'] = 'None'
 
-groupIdToName = pd.Series(df_marketGroups.marketGroupName.values, index=df_marketGroups.marketGroupID).to_dict()
-groupIdToName[-1] = 'None'
-print(df_marketGroups)
+df_marketGroups['marketGroupID'] = df_marketGroups['marketGroupID'].astype('string')
 
-df_marketGroups['parentGroupID'] = df_marketGroups['parentGroupID'].replace('None', -1)
-df_marketGroups['parentGroupID'] = df_marketGroups['parentGroupID'].astype('int64')
+marketGroups = df_marketGroups['marketGroupID'].tolist()
+parentGroups = df_marketGroups['parentGroupID'].tolist()
+# Convert groups to their names from IDs
+# parentGroups = [idToName[k] for k in parentGroups]
+# Join parent with child in single list
+zippedGroups = list(zip(parentGroups, marketGroups))
+# Get our root groups
+root_nodes = {x[1] for x in zippedGroups if x[0] == "None"}
 
-df_marketGroups['parentGroupName'] = df_marketGroups['parentGroupID'].map(groupIdToName)
+
+def get_nodes(node):
+    d = {}
+    # d['id'] = node
+    children = get_children(node)
+    if children:
+        dicts = [get_nodes(child) for child in children]
+        children = [idToName[child] for child in children]
+        d = dict(zip(children, dicts))
+    else:
+        print(type(node))
+        d = dict.fromkeys(marketGroupTypes[node])
+    return d
 
 
-# with open('eveme/static/json/marketGroups.json', 'w') as fp:
-#    json.dump(groupsJson, fp, indent=4, sort_keys=True)
+def get_children(node):
+    return [x[1] for x in zippedGroups if x[0] == node]
+
+
+tree = get_nodes('None')
+print(sorted(root_nodes))
+
+# groups = tree['None']
+# Not working for group 1697
+
+with open('eveme/static/json/marketGroups.json', 'w') as fp:
+    json.dump(tree, fp, indent=4, sort_keys=True)
