a34c9fd70ab0a979fbccade8ac821e145308dc56
nathmart <nathmart@umich.edu>
2021-09-25 19:11:34 -0700

    Updated structure mod more nice

--- eveme/helper.py
+++ eveme/helper.py
@@ -339,3 +339,21 @@ def send_token_request(form_values, add_headers={}):
     res.raise_for_status()
     print("--- send_token_request() with took %s seconds ---" % (time.time() - start_time))
     return res
+
+
+def structNameFromID(structID):
+    """Gets structure name given ID.
+
+    Args:
+        structID: A given structure ID
+    Returns:
+        structName: A string containing the structure name, returns none if doesn't exist
+    """
+    start_time = time.time()
+
+    res = requests.get("https://esi.evetech.net/latest/universe/structures/{}/".format(structID))
+
+    if res.status_code == 200:
+        return res.json()['name']
+    else:
+        return None
